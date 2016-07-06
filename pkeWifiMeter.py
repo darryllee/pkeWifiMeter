8665d8ff1da5318f687a65d826b8bda205f5013d
telarium <andrew@langleycreations.com>
2016-07-06 23:29:55 +0000

    More stuff

--- pkeWifiMeter.py
+++ pkeWifiMeter.py
@@ -1,3 +1,8 @@
-from pkeWifiMeter_wirelssList import WirelessList
+import time
+from pkeWifiMeter_wirelessList import WirelessList
 
 wifi = WirelessList()
+time.sleep(1)
+wifi.update()
+time.sleep(1)
+wifi.shutdown()
--- pkeWifiMeter_wirelessList.py
+++ pkeWifiMeter_wirelessList.py
@@ -1,18 +1,36 @@
 import csv
 import subprocess
 import os
+import sys
+import glob
 
-Class WirelessList:
+class WirelessList:
 	def __init__(self):
 		print "hi!"
+		subprocess.call(['sudo rm pkeMeterLog*.csv > /dev/null 2>&1'],shell=True)
 		airodump = subprocess.Popen(['sudo airodump-ng wlan1 -w pkeMeterLog --output-format csv > /dev/null 2>&1'],shell=True)
 
-	def shutdown():
+	def shutdown(self):
 		os.system('sudo pkill -9 airodump-ng')
+		#subprocess.call(['sudo rm pkeMeterLog*.csv > /dev/null 2>&1'],shell=True)
 
-	def getNearestDeviceName():
+	def update(self):
+		filepath = "pkeMeterLog*.csv"
+		files = glob.glob(filepath)
+		for file in files:
+			print file
+			f = open(file, 'r')
+			print f
+			try:
+    				reader = csv.reader(f)
+    				for row in reader:
+        				print row
+			finally:
+    				f.close()
+
+	def getNearestDeviceName(self):
 		print "hi!"
 
-	def getNearestDeviceStrength():
+	def getNearestDeviceStrength(self):
 		print "HI!"
 
