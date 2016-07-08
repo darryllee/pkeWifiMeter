8584578a96d6d3292fe09e8c6b3e4bf6ad2ac4a1
telarium <andrew@langleycreations.com>
2016-07-08 00:56:48 +0000

    More tweaks

--- pkeWifiMeter.py
+++ pkeWifiMeter.py
@@ -1,14 +1,40 @@
+import pygame
 import time
 from pkeWifiMeter_wirelessList import WirelessList
 
+# initialize game engine
+pygame.init()
+# set screen width/height and caption
+size = [480, 272]
+#screen = pygame.display.set_mode(size)
+#pygame.display.set_caption('PKE Wifi Meter')
+# initialize clock. used later in the loop.
+clock = pygame.time.Clock()
+
 wifi = WirelessList()
-time.sleep(5)
-wifi.update()
-time.sleep(3)
-wifi.update()
-time.sleep(3)
-wifi.update()
-time.sleep(5)
-wifi.update()
-time.sleep(3)
+
+# Loop until the user clicks close button
+done = False
+while done == False:
+    # write event handlers here
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+    # write game logic here
+ 
+    # clear the screen before drawing
+    #screen.fill((255, 255, 255)) 
+
+   # pygame.display.update()
+    
+    # fps
+    clock.tick(60)
+    wifi.update()
+    print wifi.getNearestDeviceName()
+    print wifi.getNearestDeviceStrength()
+ 
 wifi.shutdown()
+# close the window and quit
+pygame.quit()
+
+wifi = WirelessList()
--- pkeWifiMeter_wirelessList.py
+++ pkeWifiMeter_wirelessList.py
@@ -3,36 +3,48 @@ import subprocess
 import os
 import sys
 import glob
+import time
+import distutils.spawn
 
 class WirelessList:
 	def __init__(self):
-		print "hi!"
+		installed = distutils.spawn.find_executable('aircrack-ng')
+		if not installed:
+			subprocess.call(['sudo','apt-get','update'])
+			subprocess.call(['sudo','apt-get','install','aircrack-ng','-y'])
+
 		self.nearestWifi = None
 		self.nearestSignal = -100
+		self.updateTime = time.time()
+		subprocess.call(['sudo','ifconfig','wlan0','down'])
+		subprocess.call(['sudo','iwconfig','wlan1','mode','monitor'])
 		subprocess.call(['sudo rm pkeMeterLog*.csv > /dev/null 2>&1'],shell=True)
-		airodump = subprocess.Popen(['sudo airodump-ng wlan1 -w pkeMeterLog --output-format csv > /dev/null 2>&1'],shell=True)
+		airodump = subprocess.Popen(['sudo airodump-ng wlan1 --write pkeMeterLog --write-interval 1 --output-format csv > /dev/null 2>&1'],shell=True)
 
 	def shutdown(self):
 		os.system('sudo pkill -9 airodump-ng')
 		subprocess.call(['sudo rm pkeMeterLog*.csv > /dev/null 2>&1'],shell=True)
 
 	def update(self):
-		filepath = "pkeMeterLog*.csv"
-		files = glob.glob(filepath)
-		for file in files:
-			f = open(file, 'r')
-			try:
-    				reader = csv.reader(f)
-    				for row in reader:
-					if len(row) == 15 and row[0] != "BSSID":
-        					signal = int(row[8][1:])
-						if signal > self.nearestSignal:
+		if( time.time() - self.updateTime > 0.25 ):
+			print( "update" )
+			self.updateTime = time.time()
+			filepath = "pkeMeterLog*.csv"
+			files = glob.glob(filepath)
+			for file in files:
+				f = open(file, 'r')
+				try:
+    					reader = csv.reader(f)
+					nearestSignal = -100
+    					for row in reader:
+						if len(row) == 15 and row[0] != "BSSID":
+        						signal = int(row[8][1:])
+							#if signal > nearestSignal:
+							nearestSignal = signal
 							self.nearestSignal = signal
 							self.nearestWifi = row[13][1:]
-							print self.nearestWifi
-							print self.nearestSignal
-			finally:
-    				f.close()
+				finally:
+    					f.close()
 
 	def getNearestDeviceName(self):
 		return self.nearestWifi
