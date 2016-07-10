365b80c080adaac79c5938bf1e77034a0336168e
telarium <andrew@langleycreations.com>
2016-07-10 22:13:10 +0000

    Fixed LED implementation

--- pkeWifiMeter_hardware.py
+++ pkeWifiMeter_hardware.py
@@ -1,4 +1,5 @@
 import Adafruit_PCA9685
+import array
 import time
 
 class Hardware:
@@ -12,8 +13,8 @@ class Hardware:
 		self.totalLEDs = 7
 		self.ledPWMvalues = []
 
-		for led in self.ledPWMvalues:
-			led = 0
+		for i in range(self.totalLEDs):
+			self.ledPWMvalues.append(0)
 
 		self.updateTime = time.time()
 		#self.pwm.set_pwm(8, 0, 2624)
@@ -22,17 +23,17 @@ class Hardware:
 		self.pwm.set_pwm(0, 0, 0)
 
 	def updateLEDs(self,signal):
-		for i in range(1,self.totalLEDs):
-			if i == self.activeLED:
-				self.ledPWMvalues[i] = self.ledPWMvalues+0.001
+		for i in range(0,self.totalLEDs):
+			if i == self.activeLED-1:
+				self.ledPWMvalues[i] = self.ledPWMvalues[i]+600
 				if self.ledPWMvalues[i] > 4000:
 					self.ledPWMvalues[i] = 4000
 			else:
-				self.ledPWMvalues[i] = self.ledPWMvalues[i]-0.001
+				self.ledPWMvalues[i] = self.ledPWMvalues[i]-600
 			 	if self.ledPWMvalues[i] < 0:
                                         self.ledPWMvalues[i] = 0
 
-			#self.pwm.set_pwm(i, 0, self.ledPWMvalues[i])
+			self.pwm.set_pwm(i+1, 0, self.ledPWMvalues[i])
 		
 		self.ledNewSpeed = (signal*.007)*-1
 
@@ -52,6 +53,6 @@ class Hardware:
 		#self.set_servo_pulse(0,700)
 
 	def update(self,signal):
-		signal = -30
+		signal = -50
 		self.updateServo(((signal + 20)*-10)/-40)
 		self.updateLEDs(signal)
